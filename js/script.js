21a023a06896285f0e003addd41d4043edc6ff33
keita280 <tanakei1115@gmail.com>
2022-10-19 05:37:08 +0900

    newsセクションSPコーディング

--- js/script.js
+++ js/script.js
@@ -5,5 +5,14 @@
 //ハンバーガーメニューの表示
 //@@@@@@@@@@@@@@@@@@@@@@@@@
 
-
+$(function() {
+  $('#toggle').on('click', function() {
+    $('body').toggleClass('is-open');
+    if ($('#humberger-text').text() === 'CLOSE') {
+      $('#humberger-text').text('MENU');
+    } else {
+      $('#humberger-text').text('CLOSE');
+    }
+  });
+});
 
